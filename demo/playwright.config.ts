bf1057d06a8d9cb6baf463696426fe86a8d08c10
rdlabo <sakakibara@rdlabo.jp>
2026-09-16 01:11:56 +0900

    ci: increase Playwright parallelism

--- demo/playwright.config.ts
+++ demo/playwright.config.ts
@@ -7,6 +7,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
+  workers: process.env['CI'] ? 4 : undefined,
   forbidOnly: !!process.env['CI'],
   retries: process.env['CI'] ? 2 : 0,
   reporter: 'html',
